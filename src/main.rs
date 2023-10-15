6207b7cb895446f87c5bcd5e2d9709fd9faed595
Graeme Read <graeme@sigma957.net>
2023-10-15 18:28:37 +0100

    Add placeholder UI

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,7 @@ fn main() {
             }),
             ..default()
         }))
+        .add_systems(Startup, (setup_camera, setup_ui))
         // See: https://bevy-cheatbook.github.io/programming/app-builder.html#quitting-the-app
         .add_systems(Update, bevy::window::close_on_esc)
         .add_systems(Update, handle_exit)
@@ -24,4 +25,37 @@ fn handle_exit(keys: Res<Input<KeyCode>>, mut exit: EventWriter<AppExit>) {
     if keys.just_pressed(KeyCode::Q) {
         exit.send(AppExit);
     }
+}
+
+// Spawns the camera that draws UI
+fn setup_camera(mut cmd: Commands) {
+    cmd.spawn(Camera2dBundle::default());
+}
+
+/// Marker component for the text that displays the current status
+#[derive(Component)]
+struct WIPText;
+
+// Spawns the UI
+fn setup_ui(mut cmd: Commands) {
+    cmd.spawn(NodeBundle {
+        style: Style {
+            width: Val::Percent(100.),
+            ..default()
+        },
+        ..default()
+    })
+    .with_children(|root| {
+        root.spawn((
+            TextBundle::from_section(
+                "Road Road (WIP)",
+                TextStyle {
+                    font_size: 24.0,
+                    color: Color::BLACK,
+                    ..default()
+                },
+            ),
+            WIPText,
+        ));
+    });
 }
